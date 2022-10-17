0278e3f20808812ad58560e7867150700ab2f00e
Andreas Haufler <aha@scireum.de>
2022-10-17 11:44:41 +0200

    Provides some initial documentation and cleanup.

---
+++ README.md
@@ -0,0 +1,132 @@
+# scireum BUZZ
+
+Die BUZZ Schnittstelle ist eine Nachrichten-orientierte API zum Verbinden von unterschiedlichen Webseiten sowie nativen
+Anwendungen wie beispielsweise Apps in Electron, Android oder iOS. Über die Schnittstelle können beispielsweise 
+Konfiguratoren oder Produkt-Auswahlhilfen (z.B. OXOMI) in Webshops integriert werden. Weiterhin kann ein Shop direkt
+mit einem Handwerker-ERP Daten austauschen.
+
+Die Nachrichten-orientierte Ansatz gewährt eine konsistente und sichere API zwischen allen Plattformen. Über die
+BUZZ Bibliothek können mehrere "Links", also Punkt-zu-Punkt Verbindungen definiert werden. Dies ist vor allem dann
+sinnvoll, wenn ein Shop-System, sowohl mit einem Handwerker-ERP kommunizieren möchte, als auch selbst Konfiguratoren
+oder andere Auswahl-Hilfen einbinden möchte.
+
+## Verwendung
+
+Nach dem Einbinden der BUZZ Bibliothek, kann ein "Connector" zu dem Haupt-Link "nach Außen" aufgebaut werden. Dieser
+wird verwendet, wenn die Webseite z.B. in einem iFrame geladen wird:
+
+```javascript
+const catalog = new buzz.Connector({
+    name: 'catalog'
+});
+```
+
+Anschließend kann geprüft werden, ob die Gegenstelle vorhanden ist und bestimmte Funktionen unterstützt:
+
+```javascript
+catalog.queryCapability('item', function() {
+    // Die Gegenstelle kann Informationen zur Artikeln bieten -> anfragen und anzeigen...
+    document.querySelectorAll('.item').forEach(function(node) {
+        catalog.call('item', {}, {item: node.dataset['item']}, function(response) {
+            node.querySelector('.price').textContent = response.payload().price;
+            node.querySelector('.availability').textContent = response.payload().availability;
+        });
+    });
+});
+```
+
+Wird aktiv selbst eine fremde Komponente eingebunden, so kann ein eigener Link eingerichtet werden:
+```javascript
+const client = new buzz.Connector({
+    name: 'shop',
+    link: 'inner'
+});
+```
+
+Dieser kann dann der Komponente mitgeteilt werden, oder an einen iFrame angedockt werden:
+```javascript
+ new buzz.installDownlink(window.catalogFrame, {link: 'inner'});
+```
+
+Alternativ kann ein iFrame auch direkt an den "Haupt-link" konfiguriert werden, wenn keine Schnittstelle "nach außen"
+benötigt wird:
+```javascript
+ new buzz.installDownlink(window.catalogFrame, {});
+```
+
+Beispiele finden sich in [shop.html](shop.html) sowie [catalog.html](catalog.html).
+
+## Standard-Nachrichten
+
+Um einen sinnvollen Austausch unterschiedlicher Systeme zu ermöglichen, werden im folgenden Nachrichten für gängige
+Geschäftsprozesse spezifiziert. Hierbei wird immer der Mindestumfang von Nachrichten angegeben, weitere Felder können
+nach Bedarf mi-übermittelt werden.
+
+### Preis und Verfügbarkeit
+
+Elemente wie Konfiguratoren oder erweiterte Artikeldatenbanken benötigen Informationen bezüglich Preis und Verfügbarkeit
+eines Artikels. Hierfür wird die "capability" bzw. der Nachrichten-Type **priceAvailability** verwendet. Um die
+Anzeige weiter anzupassen, können Artikelnummer, Type und Kurztext überschrieben werden (um so z.B. eine Herstellernummer
+durch eine eigene Artikelnummer zu ersetzen).
+
+**Request**
+* version: Versionsnummer der Nachricht. Derzeit immer "1".
+* supplierNumber: Gibt die Herstellernummer/Lieferantennummer an, falls bekannt. Falls ein eigener Artikel angefragt wird, kann hier "-" verwendet werden. Falls die Herstellernummer unbekannt ist, kann der Parameter weggelassen werden.
+* itemNumber: Gibt die Artikelnummer an, für die Informationen geliefert werden.
+
+**Response**
+* itemNumber: Die effektive Artikelnummer die angezeigt werden soll (optional)
+* model: Die effektive Type die angezeigt werden soll (optional)
+* shortText: Der effektive Kurztext der angezeigt werden soll (optional)
+* availability
+* availabilityMessage
+* primaryPrice
+* secondaryPrice
+
+### Auskunft zu Artikeldaten bereitstellen
+
+Neben der reinen Anzeige von Preis- und Verfügbarkeit, können mit **itemData** noch weitere Informationen abgefragt
+werden. Die Anfrage ist hierbei gleich wie bei **priceAvailability**.
+
+**Response**
+* *Felder aus **priceAvailability** Antwort*
+* previewImageUrl: Url zu einem Vorschaubild (optional)
+* datasheetUrl: Url zu einem Datenblatt / Produkt-Detailseite (optional)
+
+### Artikel in Warenkorb übernehmen
+
+Um Artikel in den Warenkorb zu legen, werden unterschiedliche Capabilities / Nachrichten verwendet:
+
+* **addItemToBasket**
+  * supplierNumber: Lieferantennummer
+  * itemNumber: Artikelnummer
+  * quantity: Menge
+  * unit: Mengeneinheit (optional)
+  * shortText: Kurzbeschreibung der Position
+  * supplierName: Name des Herstellers / der Marke
+  * previewImageUrl: Vorschaubild für den Artikel
+
+## Lizenz
+```
+MIT License
+
+Copyright (c) 2022 scireum GmbH
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
--- buzz.js
+++ buzz.js
@@ -1,85 +1,142 @@
-(function (scireum) {
+/**
+ * MIT License
+ *
+ * Copyright (c) 2022 scireum GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+(function (buzz) {
 
+    /**
+     * Contains the name of the default uplink being used.
+     * @type {string}
+     */
     const LINK_NAME_BUZZ_ROOT = 'buzzRoot';
-    const ID_PREFIX = Math.round(1000000*Math.random()).toString(36);
+
+    /**
+     * Contains the name of the special link which is used by up- and downlinks to communicate with each other. Note
+     * that message un this link are then auto-mapped to the proper "outside" link.
+     * @type {string}
+     */
+    const LINK_NAME_UPLINK = 'uplink';
+
+    /**
+     * Defines the special message type used for request/response pattern.
+     *
+     * These patterns are created by "Connector.call" and "Message.reply".
+     * @type {string}
+     */
+    const MESSAGE_TYPE_RESPONSE = 'response';
+
+    /**
+     * Defines the special message type which is used to query if the peer supports a capability/message-type.
+     * @type {string}
+     */
+    const MESSAGE_TYPE_HAS_CAPABILITY = 'has-capability';
+
+    /**
+     * Used as prefix to generate unique message IDs.
+     * @type {string}
+     */
+    const ID_PREFIX = Math.round(1000000 * Math.random()).toString(36);
+
+    /**
+     * Used as sequence counter to generate unique message IDs.
+     * @type {number}
+     */
     let idCounter = 0;
 
-    function generateId() { 
-      return ID_PREFIX + '-' + (idCounter++);
+    /**
+     * Generates a unique message ID.
+     *
+     * These IDs are mostly required in request/response scenarios.
+     *
+     * @returns {string}
+     */
+    function generateId() {
+        return ID_PREFIX + '-' + (idCounter++);
     }
-    
+
+    /**
+     * Determines if a given JSON object is a message which belongs to the given buzz link.
+     *
+     * @param message the message to check
+     * @param link the link to check for
+     * @returns {boolean} true if the message belongs to the link, false otherwise
+     */
     function isBuzzMessage(message, link) {
         return message.buzzLink === link;
     }
 
-    function BuzzDebugger() {
-        window.addEventListener('message', function(event) { 
-            try {
-                const data = JSON.parse(event.data);
-                if (data.buzzLink) {
-                    console.log('scireum BUZZ Message on ' + data.buzzLink + ' from ' + data.sender, data);
-                } 
-            } catch(ignored) {}
-        });
-    }
-
-    function BuzzUplink(childFrame, options) {
-        const link = options.link || LINK_NAME_BUZZ_ROOT;
-        const extensions = options.extensions || {};
-        console.log('scireum BUZZ - Installing an uplink for bus ' + link + '... ', window, childFrame.contentWindow);
-        window.addEventListener('message', function(event) { 
-            if (event.source == window) {
-                try {
-                    const data = JSON.parse(event.data);
-                    if (isBuzzMessage(data, link) && !data.uplink) {
-                        data.buzzLink = LINK_NAME_BUZZ_ROOT;
-                        childFrame.contentWindow.postMessage(JSON.stringify(data), '*');
-                    } 
-                } catch(ignored) {}
-            } else if (event.source == childFrame.contentWindow) {
-                try {
-                    const data = JSON.parse(event.data);
-                    if (isBuzzMessage(data, 'uplink')) {
-                        data.buzzLink = link;
-                        data.uplink = true;
-                        for (const key in extensions) {
-                            data.payload[key] = extensions[key];
-                        }
-                        window.postMessage(JSON.stringify(data), '*');
-                    } 
-                } catch(ignored) {}
-            }
-        });
-
-        if (link !== LINK_NAME_BUZZ_ROOT) {
-            childFrame.contentWindow.postMessage(JSON.stringify({
-                command: 'rename-buzz-link',
-                link: link
-            }), '*');
-        }
-    }
-
-    function BuzzMessage(connector, message) {
+    /**
+     * Creates a new message for the given connector.
+     *
+     * @param connector the connector which received the message
+     * @param message the message as JSON object
+     * @constructor
+     */
+    buzz.Message = function (connector, message) {
         this.connector = connector;
         this.message = message;
     }
 
-    BuzzMessage.prototype.payload = function() {
+    /**
+     * Extracts the actual payload from the given message.
+     *
+     * @returns {*} the payload as JSON object
+     */
+    buzz.Message.prototype.payload = function () {
         return this.message.payload;
     }
-    BuzzMessage.prototype.envelope = function() {
+
+    /**
+     * Returns the message envelope itself.
+     *
+     * @returns {*} the message envelope
+     */
+    buzz.Message.prototype.envelope = function () {
         return this.message;
     }
 
-    BuzzMessage.prototype.reply = function(payload) {
-        this.connector.sendMessage('response', {
+    /**
+     * Sends a reply for this message.
+     *
+     * @param payload the payload as JSON object
+     */
+    buzz.Message.prototype.reply = function (payload) {
+        this.connector.sendMessage(MESSAGE_TYPE_RESPONSE, {
             reply: this.message.messageId,
             receiver: this.message.sender
         }, payload);
     }
-    
-    function BuzzConnector(options) {
-        this.programIdentifier = options.identifier || generateId();
+
+    /**
+     * Creates a new buzz connector.
+     *
+     * @param options provides options to properly configure the connector
+     * @param options.identifier the identifier of the software product, which is shown to the peer.
+     * @param options.name the name to be show to the peer. This could be the name of the system.
+     * @param options.link the name of the link to communicate on. This can be left empty, to use the default link
+     * (to talk to outside callers via an uplink, or a dedicated name which is forwarded via a downlink).
+     * @constructor
+     */
+    buzz.Connector = function (options) {
         this.uid = generateId();
         this.name = options.name || this.uid;
         this.options = options;
@@ -88,42 +145,58 @@
         this.waitingCalls = {};
 
         const _me = this;
-        this.addCapability('has-capability', function(message) {
+        this.addCapability(MESSAGE_TYPE_HAS_CAPABILITY, function (message) {
             if (_me.capabilities.hasOwnProperty(message.payload().capability)) {
                 message.reply({
                     uid: _me.uid,
                     name: _me.name
-                });    
+                });
             }
         });
-        this.addCapability('response', function(message) {
+        this.addCapability(MESSAGE_TYPE_RESPONSE, function (message) {
             if (_me.waitingCalls.hasOwnProperty(message.envelope().reply)) {
                 _me.waitingCalls[message.envelope().reply](message);
-                delete  _me.waitingCalls[message.envelope().reply];
+                delete _me.waitingCalls[message.envelope().reply];
             }
         });
-    
-        window.addEventListener('message', function(event) { 
+
+        window.addEventListener('message', function (event) {
             try {
                 const data = JSON.parse(event.data);
-                if (isBuzzMessage(data, _me.link) && data.sender != _me.uid && (!data.receiver || data.receiver == _me.uid)) {
+                if (isBuzzMessage(data, _me.link) && data.sender !== _me.uid && (!data.receiver || data.receiver === _me.uid)) {
                     const callback = _me.capabilities[data.type];
                     if (callback != null) {
-                        callback(new BuzzMessage(_me, data));
+                        callback(new buzz.Message(_me, data));
                     }
-                } 
-            } catch(ignored) {}
-        });   
+                }
+            } catch (ignored) {
+            }
+        });
     }
 
-    BuzzConnector.prototype.addCapability = function(capability, callback) {
+    /**
+     * Registers a capability.
+     * @param capability the name of the capability
+     * @param callback the callback to invoke for incoming messages. This will receive a "Message" object.
+     */
+    buzz.Connector.prototype.addCapability = function (capability, callback) {
         this.capabilities[capability] = callback;
     }
-    
-    BuzzConnector.prototype.sendMessage = function(type, envelope, payload) {
+
+    /**
+     * Sends a message with the given payload.
+     *
+     * @param type the message type / capability to invoke
+     * @param envelope the envelope to use for the message. This will most probably be left empty, as to common fields
+     * of the envelope are managed by the library itself.
+     * @param payload the payload to send
+     * @returns {string} the ID of the message sent
+     */
+    buzz.Connector.prototype.sendMessage = function (type, envelope, payload) {
         const message = envelope || {};
         message.type = type;
         message.sender = this.uid;
+        message.senderName = this.name;
         message.buzzLink = this.link;
         message.messageId = generateId();
         message.payload = payload;
@@ -132,39 +205,148 @@
 
         return message.messageId;
     }
-    
-    BuzzConnector.prototype.call = function(type, envelope, payload, callback) {
+
+    /**
+     * Calls the given capability and handles the response in the given callback.
+     *
+     * @param type the message type / capability to invoke
+     * @param envelope the envelope to use for the message. This will most probably be left empty, as to common fields
+     * of the envelope are managed by the library itself.
+     * @param payload the payload to send
+     * @param callback the callback to invoke once a response is received. This will receive a "Message" object.
+     * @returns {string} the ID of the message sent
+     */
+    buzz.Connector.prototype.call = function (type, envelope, payload, callback) {
         const messageId = this.sendMessage(type, envelope, payload);
         this.waitingCalls[messageId] = callback;
     }
-    
-    BuzzConnector.prototype.queryCapability = function(capability, callback) {
-        this.call('has-capability', {}, {capability: capability}, callback);
+
+    /**
+     * Determines if the peer supports a given capability.
+     * @param capability the name of the capability to check
+     * @param callback the callback which is invoked if the peer supports the requested capability
+     */
+    buzz.Connector.prototype.queryCapability = function (capability, callback) {
+        this.call(MESSAGE_TYPE_HAS_CAPABILITY, {}, {capability: capability}, callback);
     }
 
-    if (!scireum.buzzUplinkInstalled && window.top != window.self) {
-        console.log('BUZZ - Installing a downlink... ', window, window.parent);
-        scireum.buzzUplinkInstalled = true;
-        window.addEventListener('message', function(event) { 
+
+    /**
+     * Enables the built-in debugger, which logs all messages to the console.
+     */
+    buzz.enableDebugger = function () {
+        window.addEventListener('message', function (event) {
             try {
                 const data = JSON.parse(event.data);
-                if (event.source != window.parent) {
-                    if (isBuzzMessage(data, LINK_NAME_BUZZ_ROOT)) {
-                        data.buzzLink = 'uplink';
-                        window.parent.postMessage(JSON.stringify(data), "*");
-                    } 
+                if (data.buzzLink) {
+                    console.log('BUZZ Message "'
+                        + data.type
+                        + '" on '
+                        + data.buzzLink
+                        + ' from '
+                        + data.sender
+                        + ' (' + data.senderName + '): '
+                        + JSON.stringify(data.payload),
+                        data);
                 }
-            } catch(ignored) {}
+            } catch (ignored) {
+            }
         });
     }
 
-    setTimeout(function() {
-        const event = new CustomEvent('buzz-ready', {});
-        document.dispatchEvent(event);
-    }, 0);
-        
-    scireum.BuzzDebugger = BuzzDebugger;
-    scireum.BuzzUplink = BuzzUplink;
-    scireum.BuzzConnector = BuzzConnector;
-    
-    }(window.scireum = window.scireum || {}));
+    /**
+     * Connects the given iFrame to a buzz link.
+     *
+     * @param childFrame the iFrame to connect
+     * @param options the options to pass in
+     * @param options.link the link to connect to
+     * @param extensions a JSON object which will be appended to the payload of each message received from the
+     * childFrame
+     */
+    buzz.installDownlink = function (childFrame, options, extensions) {
+        const link = options.link || LINK_NAME_BUZZ_ROOT;
+        console.log('scireum BUZZ - Installing a downlink for bus ' + link + '... ', window, childFrame.contentWindow);
+        window.addEventListener('message', function (event) {
+            if (event.source === window) {
+                // Send messages to child window...
+                try {
+                    const data = JSON.parse(event.data);
+                    if (isBuzzMessage(data, link) && !data.uplink) {
+                        data.buzzLink = LINK_NAME_BUZZ_ROOT;
+                        childFrame.contentWindow.postMessage(JSON.stringify(data), '*');
+                    }
+                } catch (ignored) {
+                    console.log(ignored);
+                }
+            } else if (event.source === childFrame.contentWindow) {
+                // Receive messages from child window...
+                try {
+                    const data = JSON.parse(event.data);
+                    if (isBuzzMessage(data, LINK_NAME_UPLINK)) {
+                        data.buzzLink = link;
+                        data.uplink = true;
+                        for (const key in extensions) {
+                            data.payload[key] = extensions[key];
+                        }
+                        window.postMessage(JSON.stringify(data), '*');
+                    }
+                } catch (ignored) {
+                    console.log(ignored);
+                }
+            }
+        });
+    }
+
+    /**
+     * Installs an uplink to forward messages to the parent window if necessary.
+     */
+    function installUplink() {
+        // If the library is initialized multiple times, we can skip this...
+        if (buzz.uplinkInstalled) {
+            return;
+        }
+
+        // If there is no parent window, we don't need an uplink...
+        if (window.top === window.self) {
+            return;
+        }
+
+        console.log('scireum BUZZ - Installing an uplink... ', window, window.parent);
+        buzz.uplinkInstalled = true;
+        window.addEventListener('message', function (event) {
+            try {
+                const data = JSON.parse(event.data);
+                if (event.source !== window.parent) {
+                    if (isBuzzMessage(data, LINK_NAME_BUZZ_ROOT)) {
+                        data.buzzLink = LINK_NAME_UPLINK;
+                        window.parent.postMessage(JSON.stringify(data), "*");
+                    }
+                }
+            } catch (ignored) {
+                console.log(ignored);
+            }
+        });
+    }
+
+    /**
+     * Emits an event so to notify all users, that buzz is fully initialized.
+     */
+    function signalReadiness() {
+        if (buzz.ready === true) {
+            return;
+        }
+        buzz.ready = true;
+
+        setTimeout(function () {
+            const event = new CustomEvent('buzz-ready', {});
+            document.dispatchEvent(event);
+        }, 0);
+    }
+
+    // ---------------------------------------------------------------------------------------------------------
+    // Initializes the library within this window...
+    // ---------------------------------------------------------------------------------------------------------
+    installUplink();
+    signalReadiness();
+
+}(window.buzz = window.buzz || {}));
